5131cc15d3c1129dd3989cdb9a6a7916aa7c3d26
Henrique Jordão Baier De Azevedo <45922978810@info.local>
2023-05-29 08:17:26 -0300

    .

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_Gamer_29052023.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
